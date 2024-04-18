f651b86c3ce97c0dcb0ce86421c6d7e4722fec2a
Dexter <anisiovalverde@gmail.com>
2024-04-18 11:18:27 -0400

    Adicionada a estrutura basica de pastas para o www e CRUD

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+import "dotenv/config"
 import express from "express";
 import app from "./src/app.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+const port = 80;
+
+app.use(express.static(__dirname + "/public_html/www"));
 app.listen(port, () => {
     console.log(`server online and listen at http://localhost:${port}`);
 });
