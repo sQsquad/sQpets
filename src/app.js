0af16b95b28dcb701c5d669c61c9c1d23b152003
IS Oguro <ingrid.shizue@gmail.com>
2024-04-22 20:29:09 -0300

    reset main

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.get("/", (req, res) => {
-    res.status(200).send("<title>sQpets</title><h1>sQpets</h1><p>Coming soon...</p>");
-})
-
-export default app;
