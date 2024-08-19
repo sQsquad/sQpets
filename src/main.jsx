62e2d6ad9cfb4dc00d05cf2feccaffb64f856f6a
Ana Karol <151869218+anakarolcatu@users.noreply.github.com>
2024-08-19 20:59:24 -0300

    add: react router, homepage, cadastro, dashboard, login pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './main.sass'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/*ADICIONAR APPROUTES AQUI */}
+    <Header/>
   </React.StrictMode>,
 )
