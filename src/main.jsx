3728cb650ea38caa4b416c3dcc9cbd269e6fb389
fernando <luizfernandosr@outlook.com.br>
2024-08-19 23:20:38 -0300

    ajustes de rotas e arquivo app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './main.sass'
+import App from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/*ADICIONAR APPROUTES AQUI */}
-    <Header/>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+      </Routes> 
+    </BrowserRouter>
+  </React.StrictMode>
 )
