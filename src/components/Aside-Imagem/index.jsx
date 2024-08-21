1409420992a3f224010560dc71447ee3123ec2eb
luckfer7 <lucasferreira059@gmail.com>
2024-08-21 15:30:55 -0300

    imagemaside feita

---
+++ src/components/Aside-Imagem/index.jsx
@@ -0,0 +1,7 @@
+import imagemIside from '../../../public/imagens-svg/aside-imagem.svg';
+
+export default function AsideImagem () {
+    return (
+        <aside> <img src={imagemIside}/></aside>
+    )
+}
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,10 +1,27 @@
 import Header from "../../components/header"
 import './homepage.css'
+import AsideImagem from '../../components/Aside-Imagem';
 
 export default function HomePage() {
 
+    // const AsideImg = AsideImagem();
+
     return (
-        <Header/>
+        <>
+            <Header />
+            <section>
+                <div className="Container">
+
+                </div>
+                <div className="Container--img">
+                    <AsideImagem />
+                </div>
+            </section>
+            
+
+        </>
+        
+
     )
     
 }
