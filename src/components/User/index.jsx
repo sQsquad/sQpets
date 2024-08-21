70310fcddec6df0b124fafe3e63fea340032f427
Fernando S. <luizfernandosr@outlook.com.br>
2024-08-21 20:58:41 -0300

    inicio de funcao usuario

---
+++ src/components/User/index.jsx
@@ -0,0 +1,15 @@
+import userAvatar from '/imagens-svg/user.svg'
+import './user.css'
+
+export default function User () {
+    return (
+        <div className='user_container'>
+            <div className='user_circle_animated'>
+                <div className='user_photo'>
+                    <img src={userAvatar} alt="imagem do usuario"/>
+                </div>
+            </div>
+        </div>
+
+    )
+}
