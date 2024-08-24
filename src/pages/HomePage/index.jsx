b679cad81c8d2b69692ce87c3544cefc8868b776
luckfer7 <lucasferreira059@gmail.com>
2024-08-24 16:49:48 -0300

    botao criar tarefa criado

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,6 +1,7 @@
 import Header from "../../components/header"
 import './homepage.css'
 import AsideImagem from '../../components/Aside-Imagem';
+import CriarTarefa from "../../components/botao-criar-tarefa";
 
 export default function HomePage() {
 
@@ -15,6 +16,7 @@ export default function HomePage() {
                 </div>
                 <div className="Container--img">
                     <AsideImagem />
+                    <CriarTarefa />
                 </div>
             </section>
             
