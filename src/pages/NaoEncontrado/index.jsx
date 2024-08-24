4abec00e0211957911edd0d88bc373deb89a8501
Ana Karol <151869218+anakarolcatu@users.noreply.github.com>
2024-08-23 22:39:30 -0300

    add: titulo

--- src/pages/NaoEncontrado/index.jsx
+++ src/pages/NaoEncontrado/index.jsx
@@ -1,7 +1,17 @@
 import Header from "../../components/header"
+import Titulo from "../../components/Titulo"
+import Botao from "../../components/Botao"
+import './naoencontrada.css';
 
 export default function NaoEncontrado() {
     return (
-        <Header />
+        <>
+            <Header />
+            <div className="naoEncontrado_principal">
+                <Titulo>OPS! Página não encontrada!</Titulo>
+                <Botao>Voltar</Botao>
+            </div>
+        </>
+        
     )
 }
