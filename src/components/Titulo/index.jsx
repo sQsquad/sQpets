33c43641d936f1805b149456626390e3a1a883fb
IS Oguro <ingrid.shizue@gmail.com>
2024-08-26 19:06:17 -0300

    Revert "add: componente titulo"
    This reverts commit 7b85901b39e6255d08e9725293a02f8844779820.

--- src/components/Titulo/index.jsx
+++ src/components/Titulo/index.jsx
@@ -1,6 +1,5 @@
 import './titulo.css';
-
-export default function Titulo({children}){
+export default function Titulo({children, cor}){
     return(
         <h1 className="titulo_padrao dark_mode">
             {children}
