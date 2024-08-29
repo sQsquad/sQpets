16e3d51e3213f96c48552cdabf6022b9fa03b3cb
Ana Karol <151869218+anakarolcatu@users.noreply.github.com>
2024-08-28 21:47:25 -0300

    alteração no tamanho do botao e pagina 404

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,11 @@
 import Header from "../../components/header";
+import CardTarefa from "../../components/CardTarefa";
 
 export default function Dashboard() {
     return (
+        <>
         <Header/>
+        <CardTarefa />
+        </>
     )
 }
--- src/pages/NaoEncontrado/index.jsx
+++ src/pages/NaoEncontrado/index.jsx
@@ -1,17 +1,12 @@
 import Header from "../../components/header"
 import Titulo from "../../components/Titulo"
-import Botao from "../../components/Botao"
 import './naoencontrada.css';
 
 export default function NaoEncontrado() {
     return (
         <>
             <Header />
-            <div className="naoEncontrado_principal">
-                <Titulo>OPS! Página não encontrada!</Titulo>
-                <Botao>Voltar</Botao>
-            </div>
+            <Titulo>OPS! Página não encontrada!</Titulo>                
         </>
-        
     )
 }
