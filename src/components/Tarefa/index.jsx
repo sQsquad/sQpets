7bdd950eceaa15d345b3e5ad5d5825b9d60b872c
Ana Karol <151869218+anakarolcatu@users.noreply.github.com>
2024-08-31 19:50:37 -0300

    troca de input

--- src/components/Tarefa/index.jsx
+++ src/components/Tarefa/index.jsx
@@ -4,7 +4,7 @@ export default function Tarefa() {
         <div className="tarefa_container">
             <div className="checkbox_container"> 
                 <input type="checkbox" className="tarefa_input" name="tarefa"/>
-                <label for="tarefa">Curso JS</label>
+                <label htmlFor="tarefa">Curso JS</label>
             </div>
             <p className="tarefa_categoria">Programação</p>
             <p className="checkbox_container">01:00</p>
--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,3 +1,12 @@
+import Header from "../../components/header";
+import Tarefa from "../../components/Tarefa";
+
 export default function Cadastro() {
-    <></>
+    return(
+        <>
+        <Header/>
+        <Tarefa/>
+        </>   
+    )
+    
 }
