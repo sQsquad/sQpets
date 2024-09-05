d978b58878d90ac6ba64c66c8e80ff44bfeb76b2
Fernando S. <luizfernandosr@outlook.com.br>
2024-09-05 17:24:12 -0300

    component pet criado, pasta aside removida

--- src/components/Aside-Imagem/index.jsx
+++
@@ -1,7 +0,0 @@
-import imagemIside from '../../../public/imagens-svg/aside-imagem.svg';
-
-export default function AsideImagem () {
-    return (
-        <aside> <img src={imagemIside}/></aside>
-    )
-}
