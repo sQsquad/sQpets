ba3a3eb541bd1eb0c0bb9dc7cd5a096730020134
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2025-03-05 18:16:57 -0300

    refactor: removendo estilização por SASS e adicionando Styled Components

--- src/components/InputCheckBox/index.jsx
+++ src/components/InputCheckBox/index.jsx
@@ -1,19 +1,55 @@
-import './inputCheckbox.sass'
+import styled from 'styled-components'
 
-export default function InputCheckbox({name, type, textLabel}) {
+const CheckboxContainer = styled.div`
+    display: flex;
+    position: relative;
+    align-items: center;
+    gap: 0.5rem;
+    min-width: 0;
+    max-width: 100%;
+    overflow: hidden;
+`
+const StyledInputCheckbox = styled.input`
+    display: flex;
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    font-family: ${(props) => props.theme['fonte']};
+    font-weight: 700;
+    color: ${(props) => props.theme['--details']};
+    background: ${(props) => props.theme['--bg-color']};
+
+    @media (max-width: 480px) {
+        width: 20px;
+        height: 20px;
+    }
+`
+const Label = styled.label`
+    display: flex;
+    align-items: center;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    max-width: 300px;
+
+    @media (max-width: 480px) {
+        max-width: 80px;
+    }
+`
+
+export default function InputCheckbox({ name, type, textLabel }) {
     return (
-        <div className='checkbox_container'>
-            <input 
-                className='input_checkbox'
-                type={type} 
-                id={name} 
+        <CheckboxContainer>
+            <StyledInputCheckbox
+                type={type}
+                id={name}
             />
-            <label
+            <Label
                 className='label'
                 htmlFor={name}
             >
                 {textLabel}
-            </label>
-        </div>
+            </Label>
+        </CheckboxContainer>
     )
 }
