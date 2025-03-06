c5b160355acb52130d494a61b0ec3b9aa2c5ef6f
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2025-03-06 19:22:25 -0300

    style: ajustando estilização da label no input de tarefas

--- src/components/InputCheckBox/index.jsx
+++ src/components/InputCheckBox/index.jsx
@@ -8,48 +8,49 @@ const CheckboxContainer = styled.div`
     min-width: 0;
     max-width: 100%;
     overflow: hidden;
-`
+    `;
+
 const StyledInputCheckbox = styled.input`
-    display: flex;
     width: 30px;
     height: 30px;
     border-radius: 10px;
-    font-family: ${(props) => props.theme['fonte']};
     font-weight: 700;
-    color: ${(props) => props.theme['--details']};
-    background: ${(props) => props.theme['--bg-color']};
-
+    
     @media (max-width: 480px) {
         width: 20px;
         height: 20px;
     }
-`
-const Label = styled.label`
+    `;
+
+const LabelWrapper = styled.div`
     display: flex;
-    align-items: center;
+    height: auto;
+    flex: 1;
+    min-width: 0;
+    `;
+
+const Label = styled.label`
+    display: block;
+    text-align: left;
     text-overflow: ellipsis;
     overflow: hidden;
     white-space: nowrap;
-    max-width: 300px;
-
+    max-width: 100%;
+    color: ${(props) => props.theme['--details']};
+    font-family: ${(props) => props.theme['fonte']};
+    
     @media (max-width: 480px) {
         max-width: 80px;
     }
-`
+`;
 
 export default function InputCheckbox({ name, type, textLabel }) {
     return (
         <CheckboxContainer>
-            <StyledInputCheckbox
-                type={type}
-                id={name}
-            />
-            <Label
-                className='label'
-                htmlFor={name}
-            >
-                {textLabel}
-            </Label>
+            <StyledInputCheckbox type={type} id={name} />
+            <LabelWrapper>
+                <Label htmlFor={name}>{textLabel}</Label>
+            </LabelWrapper>
         </CheckboxContainer>
     )
 }
